dd9dfb4123f9019848503936c1e69d4a47b05d1a
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-05-22 15:27:20 +0700

    src code
    khởi tạo src code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { AppRoutingModule } from './app-routing.module'; // Import AppRoutingModule
-import { HeaderComponent } from './admin/header/header.component';
-import { HomeComponent } from './admin/home/home.component'; // Import HomeComponent
-=======
 
 import { HeaderComponent } from './admin/header/header.component';
 
@@ -13,26 +10,13 @@ import { HeaderComponent } from './admin/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './admin/home/home.component';
 import { FooterComponent } from './admin/footer/footer.component';
->>>>>>> 3a5b3f8d5b5b1efac4f485f2f12547a9957ebd2c
 import { ContentComponent } from './admin/content/content.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     HeaderComponent,
-    HomeComponent,
-    ContentComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule // Thêm AppRoutingModule vào imports
-=======
-    CategoriesComponent,
-    HeaderComponent,
-    StarComponent,
-    ProductListComponent,
-    ParentComponent,
-    ChildComponent,
     HomeComponent,
     FooterComponent,
     ContentComponent,
@@ -41,7 +25,6 @@ import { ContentComponent } from './admin/content/content.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule // Add FormsModule here
->>>>>>> 3a5b3f8d5b5b1efac4f485f2f12547a9957ebd2c
   ],
   providers: [],
   bootstrap: [AppComponent]
