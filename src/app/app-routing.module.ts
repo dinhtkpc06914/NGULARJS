3636a29050b42204a2a3022abef9313612696f82
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-05-23 00:34:36 +0700

    xoa module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './admin/home/home.component';
-import { HeaderComponent } from './admin/header/header.component';
+
 import { ContentComponent } from './admin/content/content.component';
 import { UiComponent } from './admin/ui/ui.component';
 import { TabPanelComponent } from './admin/tab-panel/tab-panel.component';
@@ -10,7 +10,7 @@ import {ChartComponent } from './admin/chart/chart.component';
 import {TableComponent } from './admin/table/table.component';
 import {FormComponent } from './admin/form/form.component';
 import {BlankComponent } from './admin/blank/blank.component';
-import { FooterComponent } from './admin/footer/footer.component';
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
