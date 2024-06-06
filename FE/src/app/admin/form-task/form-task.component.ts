bb3e4a1f2acdccfc3d5daed369e524ad04498267
lequocan948 <132972274+lequocan948@users.noreply.github.com>
2024-06-06 17:14:54 +0700

    chức năng thêm và xóa task nhưng thêm còn tí lỗi tí fix
    hehehehe

--- FE/src/app/admin/form-task/form-task.component.ts
+++ FE/src/app/admin/form-task/form-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms'
+import { TaskService } from '../services/task.service';
+import { ITask } from '../entities/task';
 
 @Component({
   selector: 'app-form-task',
@@ -8,12 +9,31 @@ import { NgForm } from '@angular/forms'
 })
 export class FormTaskComponent implements OnInit {
 
-  constructor() { }
+  task: ITask = {
+    project_id: '',
+    name: '',
+    description: '',
+    assignee_id: '',
+    status: '',
+    priority: '',
+    start_date: '',
+    due_date: ''
+  };
 
-  ngOnInit() {
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {}
+
+  onSubmit() {
+    this.taskService.createtask(this.task).subscribe(
+      response => {
+        console.log('Task added successfully', response);
+        // Thêm hành động cần thiết sau khi thêm task thành công, như điều hướng hoặc hiển thị thông báo
+      },
+      error => {
+        console.error('Error adding task', error);
+        // Thêm xử lý lỗi nếu cần
+      }
+    );
   }
-
-  onSubmit(contactForm:NgForm ) {
-    console.log(contactForm.value ) ;
-    }
 }
