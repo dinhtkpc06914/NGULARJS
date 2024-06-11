0d49d5ece941752f40c40b7aefd51787f4b64661
dinhtkpc06914 <132972173+dinhtkpc06914@users.noreply.github.com>
2024-06-12 00:54:37 +0700

    lỗi

--- FE/src/app/services/task.service.ts
+++ FE/src/app/services/task.service.ts
@@ -4,26 +4,28 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { ITask } from '../entities/task'
 
 @Injectable()
 export class TaskService {
   private url = 'http://localhost:3000/api/tasks';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
-  getAlltasks(): Observable<any[]> {
-    return this.http.get<any[]>(this.url)
-      .map(response => response as any[])
+  getAlltasks(): Observable<ITask[]> {
+    return this.http.get<ITask[]>(this.url)
+      .map(response => response as ITask[])
       .catch(this.handleError);
   }
 
-
-  getTaskById(id: string): Observable<any> {
+  getTaskById(id: string): Observable<ITask> {
     if (!id) {
       return Observable.throw('ID không hợp lệ');
     }
-    return this.http.get<any>(`${this.url}/${id}`)
-    .catch(this.handleError);
+    return this.http.get<ITask>(`${this.url}/${id}`)
+      .catch(this.handleError);
   }
 
   
@@ -33,16 +35,14 @@ export class TaskService {
       .catch(this.handleError);
   }
 
-
-  createtask(task:any): Observable<any> {
-    return this.http.post(`${this.url}`, task);
+  createtask(post:any): Observable<any> {
+    return this.http.post(`${this.url}`, post);
   }
 
-
-  updatetask(task: any): Observable<any> {
+  updatetask(task: ITask): Observable<ITask> {
     if (task._id) {
       const taskId = task._id;
-      return this.http.put<any>(`${this.url}/${taskId}`, task)
+      return this.http.put<ITask>(`${this.url}/${taskId}`, task)
         .catch(this.handleError);
     } else {
       console.error('ID task không hợp lệ!');
